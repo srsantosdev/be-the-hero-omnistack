df66fe7ab37b50985869d56798211f940ead70cd
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-03-27 16:03:43 -0300

    validações e testes backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+const { errors } = require("celebrate");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(process.env.PORT, () => {
-  console.log("Server online");
-});
+module.exports = app;
